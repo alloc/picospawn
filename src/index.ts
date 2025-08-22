87fc9431552d720e2ec52e1ee11cc3346b95beac
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2025-08-22 13:21:44 -0400

    Revert "chore(refactor): stop using Proxy to inherit promise methods"
    This reverts commit 46d0eca9e32b3dc22e501d60af2039878494f806.

--- src/index.ts
+++ src/index.ts
@@ -182,11 +182,14 @@ export const createSpawn: {
       })
     })
 
-    return Object.assign(proc, {
-      then: promise.then.bind(promise) as any,
-      catch: promise.catch.bind(promise) as any,
-      finally: promise.finally?.bind(promise) as any,
-      [Symbol.toStringTag]: 'PicospawnPromise',
+    return new Proxy(promise, {
+      get(target: any, prop: string | symbol) {
+        const resolve = (obj: any, prop: string | symbol, value = obj[prop]) =>
+          typeof value === 'function' ? value.bind(obj) : value
+
+        const value = resolve(proc, prop)
+        return value !== undefined ? value : resolve(target, prop)
+      },
     }) as PicospawnPromise
   }
 
